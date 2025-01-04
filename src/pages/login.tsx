ea6b15efb06237ab196ceb6887994e81cd264fbb
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-01-05 01:16:54 +0900

    feat : 로그인 기능 로직 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,48 @@
+import { useRouter } from "next/router";
 import style from "./login.module.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
-  const onHandleSignUp = () => {};
+  const [formData, setFormData] = useState({
+    nickname: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+  };
+  const onHandleLogin = async () => {
+    if (!formData.nickname || !formData.password) {
+      setError("닉네임과 비밀번호를 입력해주세요.");
+      return;
+    }
+
+    try {
+      const response = await axios.post("/users/login", {
+        nickname: formData.nickname,
+        password: formData.password,
+      });
+
+      // 로그인 성공 시 상태 저장 및 리다이렉트
+      localStorage.setItem("user", JSON.stringify(response.data));
+      alert("로그인 성공!");
+      router.push("/"); // 홈페이지로 이동
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data?.message || "로그인에 실패했습니다.");
+      } else {
+        setError("서버와 연결할 수 없습니다.");
+      }
+    }
+  };
+  const onHandleRedirectToSignUp = () => {
+    router.push("/signup");
+  };
+
   return (
     <div className={style.container}>
       <section>
@@ -15,6 +56,8 @@ export default function Login() {
             name="nickname"
             placeholder="닉네임을 입력해주세요..."
             className={style.input_data}
+            value={formData.nickname}
+            onChange={onHandleChange}
           />
         </div>
       </section>
@@ -26,9 +69,19 @@ export default function Login() {
             name="password"
             placeholder="비밀번호를 입력해주세요..."
             className={style.input_data}
+            value={formData.password}
+            onChange={onHandleChange}
           />
-          <button onClick={onHandleSignUp} className={style.login_button}>
-            Login
+          <button onClick={onHandleLogin} className={style.login_button}>
+            로그인
+          </button>
+          {error && <p className={style.error}>{error}</p>}
+          <hr />
+          <button
+            onClick={onHandleRedirectToSignUp}
+            className={style.login_button}
+          >
+            계정이 없으신가요?
           </button>
         </div>
       </section>
