54b790b4acd7f2a2600e740171c6d00238354119
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-01-06 22:03:36 +0900

    feat : 네비게이션 바 생성

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { useAuthStore } from "../entities/user/authStore";
 import { useRouter } from "next/router";
+import NavigationBar from "@/widgets/header";
 
 export default function Home() {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn); // 로그인 상태
-  const logout = useAuthStore((state) => state.logout);
   const router = useRouter();
 
   useEffect(() => {
@@ -13,21 +13,12 @@ export default function Home() {
     }
   }, [isLoggedIn, router]);
 
-  const handleLogout = () => {
-    logout(); // Zustand 상태 초기화
-    localStorage.removeItem("user"); // 로컬 스토리지에서 사용자 정보 제거
-    localStorage.removeItem("token"); // (JWT 사용 시) 토큰 제거
-    router.push("/login"); // 로그인 페이지로 리다이렉트
-  };
   return (
-    <>
-      <h1>hello, Next!</h1>
-      <button
-        onClick={handleLogout}
-        style={{ marginTop: "20px", padding: "10px" }}
-      >
-        로그아웃
-      </button>
-    </>
+    <div>
+      <NavigationBar />
+      <div style={{ paddingTop: "60px" }}>
+        <h1>hello, Next!</h1>
+      </div>
+    </div>
   );
 }
---
+++ src/widgets/header.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import { useAuthStore } from "../entities/user/authStore";
+import style from "./header.module.css";
+
+export default function NavigationBar() {
+  const router = useRouter();
+  const logout = useAuthStore((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    alert("로그아웃 되었습니다.");
+    router.push("/login");
+  };
+
+  return (
+    <nav className={style.navigation}>
+      <h1 className={style.logo} onClick={() => router.push("/")}>
+        웹서비스 이름
+      </h1>
+
+      <div className={style.menu}>
+        <button
+          className={style.menu_item}
+          onClick={() => router.push("/allsheets")}
+        >
+          변환된 악보
+        </button>
+        <button className={style.menu_item} onClick={handleLogout}>
+          로그아웃
+        </button>
+      </div>
+    </nav>
+  );
+}
