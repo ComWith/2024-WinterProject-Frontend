8a98b7adabde5f33f87187e74558118dfba26649
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-02-06 16:18:32 +0900

    fix: PDF 미리보기가 아닌 다운로드가 되는 문제 해결 (Google Docs Viewer 적용)

--- src/pages/allsheets/[sheet_id].tsx
+++ src/pages/allsheets/[sheet_id].tsx
@@ -152,7 +152,9 @@ export default function MusicSheetPage() {
         {showPreview && (
           <div className={style.preview}>
             <iframe
-              src={musicsheet.pdf_url}
+              src={`https://docs.google.com/gview?embedded=true&url=${encodeURIComponent(
+                musicsheet.pdf_url
+              )}`}
               width="100%"
               height="600px"
               className={style.iframe}
